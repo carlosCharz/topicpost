270b8dd388bb225e702d919a698623f01f6faa36
Carlos Becerra <carlos.becerra1234@gmail.com>
2021-01-23 17:33:29 -0500

    initial commit

---
+++ src/main/java/com/wedevol/topicpost/TopicpostApplication.java
@@ -0,0 +1,13 @@
+package com.wedevol.topicpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedevol/topicpost/TopicpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedevol.topicpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
