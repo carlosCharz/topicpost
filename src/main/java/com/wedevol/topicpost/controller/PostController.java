a05240520f116188a7f1981e348bd0583898aff8
Carlos Becerra <carlos.becerra1234@gmail.com>
2021-01-24 10:47:44 -0500

    post controller

---
+++ src/main/java/com/wedevol/topicpost/controller/PostController.java
@@ -0,0 +1,23 @@
+package com.wedevol.topicpost.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.wedevol.topicpost.dao.PostRepository;
+import com.wedevol.topicpost.model.Post;
+
+@Controller
+@RequestMapping(path = "/post")
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+}
